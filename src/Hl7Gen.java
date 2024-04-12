8654e050f7c0acddff0c1168ca48941423f5e933
ChunboC <dev@chunboc.com>
2024-04-12 15:09:00 -0400

    added features to update instance variables

--- src/Hl7Gen.java
+++ src/Hl7Gen.java
@@ -2,35 +2,68 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Random;
 import java.util.UUID;
-
 /**
+ * @version 2.0
  * @author Chunbo Cheng
  */
 public class Hl7Gen {
     private String todayStr;
-    private String name;
+    private String firstName = "test";
+    private String lastName;
     private int accession;
     private int mrnNum;
+    Random rand = new Random();
 
     public Hl7Gen() {
-        Random rand = new Random();
-        name = UUID.randomUUID().toString().substring(0,4) + "^" + "test";
+        lastName = UUID.randomUUID().toString().substring(0,8);
         LocalDate today = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
         todayStr = today.format(formatter);
-        accession = rand.nextInt(1, 100000);
-        mrnNum = rand.nextInt(1, 100000);
+        accession = rand.nextInt(1, 10000000);
+        mrnNum = rand.nextInt(1, 10000000);
     }
 
-    public void setName(int num) {
-        name += num + "";
+    public void incrementFirstName(int num) {
+        firstName += num;
+    }
+
+    public void randomMRN() {
+        mrnNum = rand.nextInt(1, 10000000);
+    }
+
+    public void randomAccession() {
+        accession = rand.nextInt(1, 10000000);
+    }
+
+    public void setLastName(String pName) {
+        lastName = pName;
+    }
+
+    public void setFirstName(String fName) {
+        firstName = fName;
+    }
+
+    public void setMRN(int mrn) {
+        mrnNum = mrn;
+    }
+
+    public void setAccession(int ass) {
+        accession = ass;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 
     public String toString() {
         String msg = "";
         msg = "\u000BMSH|^~\\&|NextGen Rosetta|NextGen Clinic^0001|Billing System|Billing System|20231206144451450||SIU^S12|2819536831|P|2.3\n" +
                 "SCH||||||CAE^CAE||CAE^CAE|20|minutes|^^^" + todayStr + "1220" + "^" + todayStr + "1240" + "|||||2295^Campbell^Sue||||2295^Campbell^Sue|||||KEPT\n" +
-                "PID|1||MRN - " + mrnNum + "||" + name + "||20000914|M||^White|555 Tarrytown Rd^^Sleepy Hollow^NY^10591^USA^^^WESTCHESTER||9148311319^^^anthony@entandallergy.com|9143335886|^English|S||" + accession + "||||^Not Hispanic or Latino||||||||N\n" +
+                "PID|1||" + mrnNum + "||" + firstName + "^" + lastName + "||20000914|M||^White|555 Tarrytown Rd^^Sleepy Hollow^NY^10591^USA^^^WESTCHESTER||9148311319^^^anthony@entandallergy.com|9143335886|^English|S||" + accession + "||||^Not Hispanic or Latino||||||||N\n" +
                 "PV1|1|O|DS-360||||1588006035^Adler AUD^Stephanie^F||||||||||||6713561|||||||||||||||||||||||||201605181020||||||6713561\n" +
                 "AIL|1|A|DS-360|^Place||201605181020||||||KEPT\n" +
                 "\u001C";
--- src/Hl7GenApp.java
+++ src/Hl7GenApp.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -6,67 +7,114 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.*;
 /**
+ * @version 2.0
  * @author Chunbo Cheng
  */
 public class Hl7GenApp {
     private static int numOperations;
-    private static String desktopPath = System.getProperty("user.home") + "/Desktop/";
-    private static String folderName = "queue/";
-    private static String folderPath = desktopPath + folderName;
-    private static File folder = new File(folderPath);
-
+    private static String filePath = "";
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            createAndShowGUI();
+        });
+    }
 
+    private static void createAndShowGUI() {
         JFrame frame = new JFrame("HL7 Sample Generator");
-        frame.setSize(450, 200);
+        frame.setSize(370, 380);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
 
         JLabel label = new JLabel("Number of files to generate:");
         label.setBounds(20, 20, 200, 30);
-        JLabel label2 = new JLabel("Customized path:");
-        label2.setBounds(20, 50, 200, 30);
+        JLabel label2 = new JLabel("Path: select a destination...");
+        label2.setBounds(20, 50, 500, 30);
         frame.add(label);
         frame.add(label2);
 
         JTextField textField = new JTextField();
-        JTextField textField1 = new JTextField();
+        JTextField fNameField = new JTextField();
+        JTextField lNameField = new JTextField();
+        JTextField mrnField = new JTextField();
+        JTextField accField = new JTextField();
         textField.setBounds(200, 20, 50, 30);
-        textField1.setBounds(200, 50, 200, 30);
+        fNameField.setBounds(170, 120, 160, 20);
+        lNameField.setBounds(170, 150, 160, 20);
+        mrnField.setBounds(170, 180, 160, 20);
+        accField.setBounds(170, 210, 160, 20);
         frame.add(textField);
-        frame.add(textField1);
+        frame.add(fNameField);
+        frame.add(lNameField);
+        frame.add(mrnField);
+        frame.add(accField);
+
+        JCheckBox incrementBox = new JCheckBox("auto increment for first name (different names)");
+        JCheckBox fNameBox = new JCheckBox("change first name");
+        JCheckBox lNameBox = new JCheckBox("change last name");
+        JCheckBox mrnBox = new JCheckBox("change mrn");
+        JCheckBox accBox = new JCheckBox("change accession");
+        incrementBox.setBounds(20, 90, 300, 20);
+        fNameBox.setBounds(20, 120, 130, 20);
+        lNameBox.setBounds(20, 150, 130, 20);
+        mrnBox.setBounds(20, 180, 130, 20);
+        accBox.setBounds(20, 210, 130, 20);
+        fNameBox.setSelected(false);
+        lNameBox.setSelected(false);
+        mrnBox.setSelected(false);
+        accBox.setSelected(false);
+        incrementBox.setSelected(true);
+        frame.add(incrementBox);
+        frame.add(fNameBox);
+        frame.add(lNameBox);
+        frame.add(mrnBox);
+        frame.add(accBox);
+
+        JButton selectFileButton = new JButton("Select File Destination");
+        selectFileButton.setBounds(90, 250, 170, 30);
+        selectFileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent a) {
+                selectFileDestination(frame);
+                label2.setText("Path: " + filePath);
+            }
+        });
+        frame.add(selectFileButton);
 
         JButton button = new JButton("Generate HL7 Files");
-        button.setBounds(80, 100, 150, 30);
+        button.setBounds(100, 290, 150, 30);
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String input = textField.getText();
-                String path = folderPath;
                 try {
                     numOperations = Integer.parseInt(input);
                     if (numOperations > 0) {
-                        JOptionPane.showMessageDialog(frame, "Generating " + numOperations + " HL7 files!\nAll sample files will be saved in queue folder on desktop.");
-
-                        if (!folder.exists()) {
-                            if (folder.mkdir()) {
-                                System.out.println("queue folder created: " + folderPath);
-                            } else {
-                                System.out.println("Failed to create folder: " + folderPath);
-                            }
-                        } else {
-                            System.out.println("Folder already exists: " + folderPath);
-                        }
-
+                        JOptionPane.showMessageDialog(frame, "Generating " + numOperations + " HL7 files!");
+                        Hl7Gen temp = new Hl7Gen();
+                        if (fNameBox.isSelected())
+                            temp.setFirstName(fNameField.getText());
+                        if (lNameBox.isSelected())
+                            temp.setLastName(lNameField.getText());
+                        if (mrnBox.isSelected())
+                            temp.setMRN(Integer.parseInt(mrnField.getText()));
+                        if (accBox.isSelected())
+                            temp.setAccession(Integer.parseInt(accField.getText()));
+                        String tempFirstName = temp.getFirstName();
+                        String filename = "";
                         for (int i = 0; i < numOperations; i++) {
                             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
                             String timestamp = dateFormat.format(new Date());
-                            String filename = "example_" + timestamp + "_" + i + ".hl7";
-                            try (BufferedWriter writer = new BufferedWriter(new FileWriter(folderPath + filename))) {
-                                Hl7Gen temp = new Hl7Gen();
-                                temp.setName(i);
+                            if (incrementBox.isSelected())
+                                filename = temp.getFirstName() + i + "_" + temp.getLastName() + "_" + timestamp + "_" + i + ".hl7";
+                            else
+                                filename = temp.getFirstName() + "_" + temp.getLastName() + "_" + timestamp + "_" + i + ".hl7";
+                            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath + "\\" + filename))) {
+                                if (incrementBox.isSelected())
+                                    temp.incrementFirstName(i);
                                 writer.write(temp.toString());
+                                temp.setFirstName(tempFirstName);
                                 //System.out.println("HL7 message has been written to the queue folder on desktop");
                             } catch (IOException ex1) {
                                 System.err.println("Error writing to file: " + ex1.getMessage());
@@ -81,7 +129,32 @@ public class Hl7GenApp {
             }
         });
         frame.add(button);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    private static void selectFileDestination(JFrame frame) {
+        // Create a file chooser
+        JFileChooser fileChooser = new JFileChooser();
+
+        // Set dialog type to select a file or directory
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        // Show the file chooser dialog
+        int result = fileChooser.showSaveDialog(frame);
+
+        // If user selects a file/directory and clicks "Save"
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+
+            // Get the path of the selected file
+            filePath = selectedFile.getAbsolutePath();
+
+            // Notify the user about the selected file path
+            //JOptionPane.showMessageDialog(frame, "File destination selected: " + filePath);
+        } else if (result == JFileChooser.CANCEL_OPTION) {
+            JOptionPane.showMessageDialog(frame, "File selection canceled.");
+        }
     }
 }
 
